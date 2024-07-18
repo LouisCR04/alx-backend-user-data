6fe0dcc77740b361cd24e059fa7de56648de9381
LouisCR04 <louiskariuki04@gmail.com>
2024-07-18 18:18:20 +0300

    pycodestyle compliance

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# auth.py
+
+""" User_Auth """
+
+
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,9 +2,6 @@
 # db.py
 
 """ User_Auth """
-
-#!/usr/bin/env python3
-""" Database for ORM """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
